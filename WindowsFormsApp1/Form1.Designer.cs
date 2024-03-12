8b340f83d1f909c4bfd4248588003450f4485413
cute1425833 <163030186+cute1425833@users.noreply.github.com>
2024-03-12 13:38:50 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.模式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.加密ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.解碼ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開啟檔案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開啟檔案ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.儲存檔案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(377, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(297, 456);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 52);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "加密";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "原始資料：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(654, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(566, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "加密條件：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 308);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "加密結果：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 308);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 15);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "解碼結果：";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(384, 456);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 52);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "解碼";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.模式ToolStripMenuItem,
+            this.開啟檔案ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(778, 27);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 模式ToolStripMenuItem
+            // 
+            this.模式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.加密ToolStripMenuItem,
+            this.解碼ToolStripMenuItem});
+            this.模式ToolStripMenuItem.Name = "模式ToolStripMenuItem";
+            this.模式ToolStripMenuItem.Size = new System.Drawing.Size(53, 26);
+            this.模式ToolStripMenuItem.Text = "模式";
+            // 
+            // 加密ToolStripMenuItem
+            // 
+            this.加密ToolStripMenuItem.Checked = true;
+            this.加密ToolStripMenuItem.CheckOnClick = true;
+            this.加密ToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.加密ToolStripMenuItem.Name = "加密ToolStripMenuItem";
+            this.加密ToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.加密ToolStripMenuItem.Text = "加密";
+            this.加密ToolStripMenuItem.Click += new System.EventHandler(this.加密ToolStripMenuItem_Click);
+            // 
+            // 解碼ToolStripMenuItem
+            // 
+            this.解碼ToolStripMenuItem.CheckOnClick = true;
+            this.解碼ToolStripMenuItem.Name = "解碼ToolStripMenuItem";
+            this.解碼ToolStripMenuItem.Size = new System.Drawing.Size(122, 26);
+            this.解碼ToolStripMenuItem.Text = "解碼";
+            this.解碼ToolStripMenuItem.Click += new System.EventHandler(this.解碼ToolStripMenuItem_Click);
+            // 
+            // 開啟檔案ToolStripMenuItem
+            // 
+            this.開啟檔案ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開啟檔案ToolStripMenuItem1,
+            this.儲存檔案ToolStripMenuItem});
+            this.開啟檔案ToolStripMenuItem.Name = "開啟檔案ToolStripMenuItem";
+            this.開啟檔案ToolStripMenuItem.Size = new System.Drawing.Size(89, 26);
+            this.開啟檔案ToolStripMenuItem.Text = "匯入/匯出";
+            this.開啟檔案ToolStripMenuItem.Click += new System.EventHandler(this.開啟檔案ToolStripMenuItem_Click);
+            // 
+            // 開啟檔案ToolStripMenuItem1
+            // 
+            this.開啟檔案ToolStripMenuItem1.Name = "開啟檔案ToolStripMenuItem1";
+            this.開啟檔案ToolStripMenuItem1.Size = new System.Drawing.Size(152, 26);
+            this.開啟檔案ToolStripMenuItem1.Text = "開啟檔案";
+            this.開啟檔案ToolStripMenuItem1.Click += new System.EventHandler(this.開啟檔案ToolStripMenuItem1_Click);
+            // 
+            // 儲存檔案ToolStripMenuItem
+            // 
+            this.儲存檔案ToolStripMenuItem.Name = "儲存檔案ToolStripMenuItem";
+            this.儲存檔案ToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.儲存檔案ToolStripMenuItem.Text = "儲存檔案";
+            this.儲存檔案ToolStripMenuItem.Click += new System.EventHandler(this.儲存檔案ToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "待解密資料：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(123, 119);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(377, 25);
+            this.textBox3.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(566, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "解密條件：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(654, 171);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 25);
+            this.textBox4.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(123, 251);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox5.Size = new System.Drawing.Size(509, 156);
+            this.textBox5.TabIndex = 18;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(123, 251);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox6.Size = new System.Drawing.Size(509, 156);
+            this.textBox6.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 576);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 模式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 加密ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 解碼ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ToolStripMenuItem 開啟檔案ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開啟檔案ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 儲存檔案ToolStripMenuItem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        
+        }
+        string zz;
+        char[] a = { ' ' };
+        FileInfo fileinfo;
+        private void button2_Click(object sender, EventArgs e)
+            
+        {
+            textBox5.Text = "";
+           
+
+
+            lock_p = "";
+
+            if (textBox1.Text == "")
+                MessageBox.Show("原始資料請勿空白");
+            else
+            {
+                
+
+                byte[] bytes = new byte[2];
+
+                int f;
+                string x;
+                x = "";
+                for (f = 0; f < textBox1.Text.Length; f++)
+                {
+                    bytes = Encoding.Unicode.GetBytes(textBox1.Text.Substring(f, 1));
+                    if (String.Format("{0:X}", bytes[1]).Length < 2)
+                    {
+                        x = "0" + String.Format("{0:X}", bytes[1]);
+                    }
+                    else x = String.Format("{0:X}", bytes[1]);
+
+
+                    if (String.Format("{0:X}", bytes[0]).Length < 2)
+                    {
+                        x += "0" + String.Format("{0:X}", bytes[0]);
+                    }
+                    else x += String.Format("{0:X}", bytes[0]);
+
+
+                    lock_p += x + " ";
+                    
+                   
+
+                }
+
+
+            }
+           
+            string za="";
+            if (textBox2.Text == "")
+                MessageBox.Show("加密條件請勿空白!!");
+            else
+            {
+                zz = "";
+                
+                string[] b = lock_p.Trim().Split(a);
+
+                for (int i = 0; i < b.Length; i++)
+                {
+                    for (int j = 0; j < b[i].Length; j++)
+                    {
+
+                    
+                            za = (Convert.ToInt32((ASC(b[i].Substring(j, 1)).ToString())) + Convert.ToInt32(textBox2.Text)).ToString();
+                            zz += za + " ";
+                  
+                    
+                    
+                    
+                    
+                    }
+                }
+               textBox5.Text = zz;
+                
+            }
+           
+
+
+
+        }
+       
+        
+        string lock_p ;
+        string end;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(textBox3.Text=="")
+            { MessageBox.Show("待解密資料請勿空白"); }
+            else if ( textBox4.Text == "")
+            {
+                MessageBox.Show("解碼條件請勿空白");
+            }
+            else
+            {
+               textBox6.Text = "";
+                string uni = "";
+                char[] a = { ' ' };
+                string[] b = textBox3.Text.Trim().Split(a);
+                int g = 0;
+                for (int i = 0; i < b.Length; i++)
+                {
+                    end = (Convert.ToInt32(b[i]) - Convert.ToInt32(textBox4.Text)).ToString();
+                    uni += Chr(Convert.ToInt32(end));
+                    g += 1;
+                    if (g == 4)
+                    {
+                        uni += " ";
+                        g = 0;
+                    }
+                }
+
+
+
+                byte[] bytes = new byte[2];
+                string[] c = uni.Trim().Split(a);
+
+
+                for (int j = 0; j < c.Length; j++)
+                {
+
+                    bytes[1] = Convert.ToByte(c[j].Substring(0, 2), 16);
+                    bytes[0] = Convert.ToByte(c[j].Substring(2, 2), 16);
+                    string result = Encoding.Unicode.GetString(bytes);
+
+                   textBox6.Text += result;
+                }
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            if(this.加密ToolStripMenuItem.Checked=true)
+            {
+                label8.Visible = false;
+                textBox6.Visible = false;
+                button3.Visible = false; 
+                label1.Visible = false;
+                textBox3.Visible = false;
+                label5.Visible = false;
+                textBox4.Visible = false;
+                mod = 1;
+            }
+            else
+            {
+                label7.Visible = false;
+                textBox5.Visible = false;
+                label4.Visible = false;
+                textBox1.Visible = false;
+                label6.Visible = false;
+                textBox2.Visible = false;
+                button2.Visible = false;
+                label8.Visible = true;
+               textBox6.Visible = true;
+                button3.Visible = true;
+                label1.Visible = true;
+                textBox3.Visible = true;
+                label5.Visible = true;
+                textBox4.Visible = true;
+                mod = 2;
+            }
+        }
+
+
+        public static char Chr(int Num)
+
+        {
+
+            char C = Convert.ToChar(Num);
+
+            return C;
+
+        }
+
+
+
+        public static int ASC(string S)
+
+        {
+
+            int N = Convert.ToInt32(S[0]);
+
+            return N;
+
+        }
+
+
+
+        public static int ASC(char C)
+
+        {
+
+            int N = Convert.ToInt32(C);
+
+            return N;
+
+        }
+        int mod=0;
+        private void 加密ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(this.加密ToolStripMenuItem.Checked=true)
+            { this.解碼ToolStripMenuItem.Checked = false;
+                label8.Visible = false;
+                textBox6.Visible = false;
+                button3.Visible = false;
+                label1.Visible = false;
+                textBox3.Visible = false;
+                label7.Visible = true;
+                textBox5.Visible = true;
+                label4.Visible = true;
+                textBox1.Visible = true;
+                label6.Visible = true;
+                textBox2.Visible = true;
+                button2.Visible = true;
+                label5.Visible = false;
+                textBox4.Visible =false;
+                mod = 1;
+            }
+            else
+            { this.解碼ToolStripMenuItem.Checked = true; }
+        }
+
+        private void 解碼ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.解碼ToolStripMenuItem.Checked = true)
+            { this.加密ToolStripMenuItem.Checked = false;
+                label7.Visible = false;
+               textBox5.Visible = false;
+                label4.Visible = false;
+                textBox1.Visible = false;
+                label6.Visible = false;
+                textBox2.Visible = false;
+                button2.Visible = false;
+                label8.Visible = true;
+               textBox6.Visible = true;
+                button3.Visible = true;
+                label1.Visible = true;
+                textBox3.Visible = true;
+                label5.Visible = true;
+                textBox4.Visible = true;
+                mod = 2;
+            }
+            else
+            { this.加密ToolStripMenuItem.Checked = true; }
+        }
+
+       private void 開啟檔案ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //不要寫在這裡
+        }
+        string path = "SECRET.txt";
+        private void 開啟檔案ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (mod==1)
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                    textBox1.Text = sr.ReadToEnd();
+                    sr.Close();
+
+                }
+            }
+
+            if (mod == 2)
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                    textBox3.Text = sr.ReadToEnd();
+                    sr.Close();
+
+                }
+            }
+
+        }
+
+        private void 儲存檔案ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string FileName = "SECRET.txt";
+            if (mod == 1)
+            {
+                
+                if (System.IO.File.Exists(FileName))
+                {
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+
+
+                        FileInfo fileinfo = new FileInfo(saveFileDialog1.FileName);
+                        StreamWriter sw = fileinfo.CreateText();
+                        sw.WriteLine(textBox5.Text);
+                        sw.Flush();
+                        sw.Close();
+                        this.Text = path;
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(FileName + " 檔案不存在");
+                    fileinfo = new FileInfo(path);
+                    StreamWriter sw = fileinfo.CreateText();
+                    sw.WriteLine(textBox5.Text);
+                    sw.Flush();
+                    sw.Close();
+                    MessageBox.Show("已建立檔案" + path);
+
+                }
+            }
+            if (mod == 2)
+            {
+                if (System.IO.File.Exists(FileName))
+                {
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+
+
+                        FileInfo fileinfo = new FileInfo(saveFileDialog1.FileName);
+                        StreamWriter sw = fileinfo.CreateText();
+                        sw.WriteLine(textBox6.Text);
+                        sw.Flush();
+                        sw.Close();
+                        this.Text = path;
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(FileName + " 檔案不存在");
+                    fileinfo = new FileInfo(path);
+                    StreamWriter sw = fileinfo.CreateText();
+                    sw.WriteLine(textBox6.Text);
+                    sw.Flush();
+                    sw.Close();
+                    MessageBox.Show("已建立檔案" + path);
+                }
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int ch;
+            ch = Convert.ToInt32(e.KeyChar);
+            if (ch == 8 )
+                return;
+            if (e.KeyChar < '0' || e.KeyChar > '9')
+                e.Handled = true;
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
